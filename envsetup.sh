21c72fe8c456bf08ed0636cab5322975141ea547
PipelinerArd <kristoferard@gmail.com>
2013-09-02 05:29:38 +0000

    Squash commits of the following: Unsecure by default 88476a875c0a3e243e8557fc7767181bc67bc200 Don't odex builds 246c3a4a6a832d67cf7d50c71f41e6aa90cd9993 Export only AOSPX builds Inline kernel building / Support for complex toolchains Remove Quick Search Box from build 5671b583f69147a29990acbf92cfe567a05dabcf Remove vendor module checks d81b6d9cb1fad09a5f81cc14f6d1f4002eb20a05 Signed-off-by: PipelinerArd <kristoferard@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -435,7 +435,11 @@ function print_lunch_menu()
     echo
     echo "You're building on" $uname
     echo
-    echo "Lunch menu... pick a combo:"
+    if [ "z${AOSPX_DEVICES_ONLY}" != "z" ]; then
+       echo "Breakfast menu... pick a combo:"
+    else
+       echo "Lunch menu... pick a combo:"
+    fi
 
     local i=1
     local choice
@@ -444,6 +448,10 @@ function print_lunch_menu()
         echo " $i. $choice "
         i=$(($i+1))
     done | column
+    
+    if [ "z${AOSPX_DEVICES_ONLY}" != "z" ]; then
+       echo "... and don't forget the bacon!"
+    fi
 
     echo
 }
